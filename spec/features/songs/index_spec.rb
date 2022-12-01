3fe5810166911ece446ff826f4554c208638b90b
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-11-30 17:22:17 -0800

    Test songs index and show

---
+++ spec/features/songs/index_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe "Songs index", type: :feature do
+
+  # As a user, 
+  # when I visit '/songs' 
+  # I see each song's title and play count
+
+  describe "As a user, " do
+    describe "When I visit /songs, " do
+      it "I see each song's title and play count" do
+        carly = Artist.create!(name: "Carly Rae Jepsen")
+        song_1 = Song.create!(title: "I really like you",
+                              length: 208,
+                              play_count: 923847,
+                              artist: carly)
+        song_2 = Song.create!(title: "Call me maybe",
+                              length: 208,
+                              play_count: 923847,
+                              artist: carly)                      
+        visit "/songs"
+        expect(page).to have_content(song_1.title)
+        expect(page).to have_content("Play count: #{song_1.play_count}")
+        expect(page).to have_content(song_2.title)
+        expect(page).to have_content("Play count: #{song_2.play_count}")
+      end
+    end
+
+#     As a user
+# When I visit a Song show page
+# Then I see the song's title, length, and play count
+    describe "When I visit /songs/show, " do
+    #   it "I see the song's title, length, and play count" do
+    #     carly = Artist.create!(name: "Carly Rae Jepsen")
+    #     song_1 = Song.create!(title: "I really like you",
+    #                           length: 208,
+    #                           play_count: 923847,
+    #                           artist: carly)
+    #     song_2 = Song.create!(title: "Call me maybe",
+    #                           length: 208,
+    #                           play_count: 923847,
+    #                           artist: carly)                      
+        visit "/songs/#{song_1.id}" 
+    #     expect(page).to have_content(song_1.title)
+    #     expect(page).to have_content("Play count: #{song_1.play_count}")
+    #     expect(page).to have_content("Length: #{song_1.length}")
+        
+    #     expect(page).to have_content(song_2.title)
+    #     expect(page).to have_content("Play count: #{song_2.play_count}")
+    #     expect(page).to have_content("Length: #{song_2.length}")
+    #   end
+    # end
+  end
+end
--- spec/features/songs/show_spec.rb
+++ spec/features/songs/show_spec.rb
@@ -14,6 +14,18 @@ RSpec.describe 'the songs show page' do
         expect(page).to_not have_content(song_2.title)
 
     end
+    it 'displays the song length' do
+        artist = Artist.create!(name: 'Carly Rae Jepson')
+        song = artist.songs.create!(title: "I really like you", length: 208, play_count: 923874)
+        song_2 = artist.songs.create!(title: "Call me maybed", length: 4209, play_count: 24958372)
+        visit "/songs/#{song.id}"
+
+        visit "/songs/#{song.id}"
+
+        expect(page).to have_content(song.length)
+        expect(page).to_not have_content(song_2.length)
+
+    end
 
     it 'displays the name of artist for the song' do
         artist = Artist.create!(name: 'Carly Rae Jepson')
@@ -24,6 +36,6 @@ RSpec.describe 'the songs show page' do
         visit "/songs/#{song.id}"
 
         expect(page).to have_content(song.title) 
-        save_and_open_page
+        # save_and_open_page
     end
 end
